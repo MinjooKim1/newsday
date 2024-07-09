58082484c334fc9c394c1264de79c29d34e7124a
minjoo Kim <haileykim206@gmail.com>
2024-07-09 09:29:24 -0700

    second commit

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 const getLatestNews = async () => {
-  const url = new URL(
-    `https://serene-cheesecake-72297b.netlify.app/top-headlines`
-  );
+  let url = `https://serene-cheesecake-72297b.netlify.app/top-headlines`;
+
   const response = await fetch(url);
   const data = await response.json();
   let news = data.articles;
