101c24710207010eaa6e73a696569d9b4c03ba2c
minjoo Kim <haileykim206@gmail.com>
2024-07-09 22:03:02 -0700

    third commit

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 const getLatestNews = async () => {
-  let url = new URL(`https://noona-times-be-5ca9402f90d9.herokuapp.com/`);
+  let url = new URL(
+    `https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines/?country=kr&pageSize=${PAGE_SIZE}`
+  );
 
   const response = await fetch(url);
   const data = await response.json();
